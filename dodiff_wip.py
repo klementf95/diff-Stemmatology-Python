63eee92e3d16817c82500883583008a07e719af1
Florian Klement <florian.klement@gmx.net>
2023-01-25 19:25:12 +0100

    if coco is not None

--- dodiff_wip.py
+++ dodiff_wip.py
@@ -17,7 +17,7 @@ mssDictList = defaultdict(list)
 mssListTemp = []
 
 
-with open('./test_data/coco-besoin.csv','r', newline='', encoding='utf-8') as fp:
+with open('./test_data/besoin_test.csv','r', newline='', encoding='utf-8') as fp:
     reader = csv.DictReader(fp, delimiter=',')
     for label in reader.fieldnames:
         fp.seek(0)
@@ -29,20 +29,7 @@ with open('./test_data/coco-besoin.csv','r', newline='', encoding='utf-8') as fp
 
 msLabelArray = list(mssDictList.keys())
 numOfMss = len(msLabelArray)
-            
-# for key in mssDictList:
-#     ms = mssDictList[key][1:]
-#     if ms and ms[-1] == ' ':
-#         ms = ms[:-1]
-#     ms = [word.replace(',','').replace('!','').replace('?','').replace('"','').replace('.','').replace('[', '').replace(']', '').replace('(', '').replace(')', '').replace(r'\s[^\s]*\*[^\s]*', ' €') for word in ms]
-#     mssDictList[key] = ms
-
-# for key, value in mssDictList.items():
-#     mssDict[key] = "".join(mssDictList[key])
     
-# for key in mssDictList:
-#     ms = [word.replace(' ','') for word in ms]
-#     mssDictList[key] = ms
     
 for key in mssDictList:
     ms = mssDictList[key][1:]
@@ -54,11 +41,36 @@ for key in mssDictList:
     ms = [word.replace(' ','') for word in ms]
     mssDictList[key] = ms
 
-print(mssDictList)
-        
 
-print(len(msLabelArray)) # print length of array
-print(msLabelArray[0]) # erste zeile
+def diff(list_text1, list_text2):
+    temp_diff = []
+    for item1, item2 in zip(list_text1, list_text2):
+        if item1 != item2:
+            if item1 is not None:
+                temp_diff.append(item1)
+            if item2 is not None:
+                temp_diff.append(item2)
+    return temp_diff
+
+def dodiff(a1, a2):
+    wordArrMs1 = a1
+    wordArrMs2 = a2
+
+    diffArray = diff(wordArrMs1, wordArrMs2)
+    dist = 0
+    for hunk in diffArray:
+        sequenceArray = hunk
+        distInHunk = 0
+        print(hunk)
+        #for sequence in sequenceArray:
+            #print(sequence)
+
+#dodiff(wordArrayMs1, wordArrayMs2)
+
+
+
+#print(len(msLabelArray)) # print length of array
+#print(msLabelArray[0]) # erste zeile
 
 
 for msIndex in range(1,len(msLabelArray)):
@@ -68,62 +80,13 @@ for msIndex in range(1,len(msLabelArray)):
         # Anzeil der Zeilen mal Spalten (eg. für den aktuellen Text, geh alle zuvor bearbeiteten durch) und mache pro Element/Text:        
         wordArrayMs1 = mssDictList[msLabelArray[msIndex]]
         wordArrayMs2 = mssDictList[msLabelArray[otherMsIndex]]
-
-####################################################################### --> verschieben, da achronologisch 
-# trickle down economics, die unsichtbare hand des marktes regelt
-# jeweils zeilenpaare werden angeschaut und dafür beide Zeilen in Frage ausgegeben:
-# A, AB, AC BC, AD BD CD
-
-#         Hier wird noch dodiff gecalled, wobei der gesamte content eines textes mit dem gesamten des andereren 
-#         ausgegegben und mit whitespaces umrahmt wird.
-        (diff
-        my $el=dodiff(\@wordArrayMs1, \@wordArrayMs2);
-        print " ".$el." ";
-        $matrix->assign($msIndex+1,$otherMsIndex+1,$el); 
-        $matrix->assign($otherMsIndex+1,$msIndex+1,$el)); 
+        el = dodiff(wordArrayMs1, wordArrayMs2)
+        print(el)
+        #print(" {} ".format(el))
+        #print("")
 
 
-
-def dodiff(a1, a2):
-    wordArrMs1 = a1
-    wordArrMs2 = a2
-
-# Copy the two arrays passed to this subroutine into 
-# the variables wordArrMs1 and wordArrMs2
-
-    # diff computes the smallest set of additions and deletions necessary to turn the first sequence into the second
-    # and returns a 2-dim array of differences (hunks with sequences); 
-    # each difference is a list of 3 elements (-/+, position of the change, string), e.g.:
-    # [ 
-    #   [ 
-    #      [ '-', 0, 'a' ] 
-    #   ],
-    #   [ 
-    #      [ '-', 8, 'n' ], 
-    #      [ '+', 9, 'p' ]
-    #   ]
-    # ]
-# Sind die Abänderungen innerhalb des Beispieloutputs in eine oder in beide Richtungen?
-# Sind die einzelnen Arrays für jede Zeile zu verstehen? Chaacter Sequences oder einzelne Zeichen?
-
-    diffArray = diff(wordArrMs1, wordArrMs2)
-
-    dist = 0
-    for hunk in diffArray:
-        sequenceArray = hunk
-        distInHunk = 0
-        for sequence in sequenceArray:
       # assigning a score to the pot. leitfehler in wlist();
-            word = " ".join(sequence) # Concatenate 3 elements of the list
-             # Remove +/- at the beginning and the digits (\d) of the position;
-             # leave only the char string
-            word = re.sub(r"^. \d+ (.+)", r"\1", word) # $1 contains the first match: (.+)
-            # substitute the entire output of one line of diff with the character at the end of it and save into word
-            #handpicked lf
-            #if($word=~/(definitio|tumore|cognoscere|proiciunt|afixia|introrsum|irruit|uisceribus|retentione|aperiant|molitam|catarticum|efficitur|sursum)/){$distInHunk+=$weight} # handpicked lf
-
-        # Als möglichen Parameter implementieren, ob wildcards aus der Gewichtung ausgenommen werden sollen
-        # Generelle Filterfunktionsmöglichkeiten?
             #if($word=~/€/){$distInHunk=-2} # €-wildcard matches anything
             
             # calibration to $weight.  
@@ -142,40 +105,29 @@ def dodiff(a1, a2):
             # für eine größere Anzahl an Abweichungen
             # Was macht scoremax?
             
-            if score.get(word) and scoremax * weight != 0:
-                distInHunk += score[word] / scoremax * weight
-            distInHunk += 1
-        dist += distInHunk
+        #     if score.get(word) and scoremax * weight != 0:
+        #         distInHunk += score[word] / scoremax * weight
+        #     distInHunk += 1
+        # dist += distInHunk
      
      # Die Zähler für die Anzahl der insgesamten Abweichungen pro Text und die für
      # die Anzahl der Seuqenzen werden vereint.
         
    #$dist=$#diffArray+1;
-    return int(dist + 0.5)
+    #return int(dist + 0.5)
 # Natürliches, kaufmännisches Runden wird simuliert.
 
-##################################
-##################################
+# jeweils zeilenpaare werden angeschaut und dafür beide Zeilen in Frage ausgegeben:
+# A, AB, AC BC, AD BD CD
 
-# Content vom Block vor dodiff, nach der Normaliserung, zum Erstellen der Püramide und Befüllen.
-# Hier mit dem fehlenden Block, der den Inhalt processiert und die Funktion called. In welcher Reiehenfolge
-# die Funktionen angeordnet wqrden sollen, wird sich noch im Testen mit wirklichem Inhalt ergeben müssen.
+#         Hier wird noch dodiff gecalled, wobei der gesamte content eines textes mit dem gesamten des andereren 
+#         ausgegegben und mit whitespaces umrahmt wird.
+        # (diff
+        # my $el=dodiff(\@wordArrayMs1, \@wordArrayMs2);
+        # print " ".$el." ";
+        # $matrix->assign($msIndex+1,$otherMsIndex+1,$el); 
+        # $matrix->assign($otherMsIndex+1,$msIndex+1,$el)); 
 
-
-#msLabelArray = []  # vermutlich nicht gebraucht, und überschreibt nur Input
-#mssDict = {}       # vermutlich nicht gebraucht, und überschreibt nur Input
-
-print(len(msLabelArray))
-print(msLabelArray[0])
-
-for msIndex in range(1, len(msLabelArray)):
-    print(msLabelArray[msIndex])
-    for otherMsIndex in range(0, msIndex):
-        wordArrayMs1 = mssDict[msLabelArray[msIndex]].split(" ")
-        wordArrayMs2 = mssDict[msLabelArray[otherMsIndex]].split(" ")
-
-        el = dodiff(wordArrayMs1, wordArrayMs2)
-        print(" {} ".format(el))
-    print("")
-    
-#####################################
+    #     el = dodiff(wordArrayMs1, wordArrayMs2)
+    #     print(" {} ".format(el))
+    # print("")

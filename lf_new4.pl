e11d31d9898e55d9bb835a76890a7b61532a7fc2
Florian Klement <florian.klement@gmx.net>
2023-01-29 20:25:02 +0100

    open points adjustment

--- lf_new4.pl
+++ lf_new4.pl
@@ -438,7 +438,7 @@ sub ratings
 sub vierer 
 {
     my ($a1, $a2, $a3, $t0, $t1, $t2, $t3, $word, $otherWord) = @_;
-    if($debug==3)
+    if($debug==1)
     {
         if ($t0 ne 1 && $t1 ne 1 && $t2 ne 1 && $t3 ne 1)
         {
